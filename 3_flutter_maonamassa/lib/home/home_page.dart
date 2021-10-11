47aab7722385744b6951f506a159e63bf8776c1d
Daniel K. Morita <daniel.morita@sotran.com.br>
2021-10-11 10:52:34 -0300

    container

--- 3_flutter_maonamassa/lib/home/home_page.dart
+++ 3_flutter_maonamassa/lib/home/home_page.dart
@@ -1,5 +1,41 @@
 import 'package:flutter/material.dart';
 
+// Aula 1
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Home Page'),
+//         backgroundColor: Colors.amber,
+//         actions: [
+//           IconButton(
+//             onPressed: () {},
+//             icon: Icon(Icons.add_link_outlined),
+//           ),
+//           IconButton(
+//             onPressed: () {},
+//             icon: Icon(Icons.airplane_ticket_outlined),
+//           ),
+//         ],
+//       ),
+//       drawer: Drawer(
+//         child: Center(
+//           child: Text('Drawer aberto'),
+//         ),
+//       ),
+//       endDrawer: Drawer(
+//         child: Center(
+//           child: Text('Drawer a direita'),
+//         ),
+//       ),
+//       body: Center(child: Text('Home Page')),
+//     );
+//   }
+// }
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -8,29 +44,32 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
-        backgroundColor: Colors.amber,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.add_link_outlined),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.airplane_ticket_outlined),
-          ),
-        ],
       ),
-      drawer: Drawer(
-        child: Center(
-          child: Text('Drawer aberto'),
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(10),
+          // color: Colors.red,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(30),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black,
+                blurRadius: 20,
+                offset: Offset(10, 10),
+              ),
+              BoxShadow(
+                color: Colors.green,
+                blurRadius: 20,
+                offset: Offset(-10, -10),
+              ),
+            ],
+          ),
         ),
       ),
-      endDrawer: Drawer(
-        child: Center(
-          child: Text('Drawer a direita'),
-        ),
-      ),
-      body: Center(child: Text('Home Page')),
     );
   }
 }
